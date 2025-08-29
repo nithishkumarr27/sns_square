16b9df8ec0fff8bc9ef0d48b67215f56c78b8101
Gowtham A <121245988+Gowtham102022@users.noreply.github.com>
2025-08-29 18:56:24 +0530

    Resolve utils.js merge conflict

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import { clsx } from "clsx";
-=======
-
-import { clsx } from "clsx";
-
->>>>>>> a4f5890de8d8cac7cf1318e5af6a039482d00171
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
-<<<<<<< HEAD
 }
-=======
-
-}
-
->>>>>>> a4f5890de8d8cac7cf1318e5af6a039482d00171
